a1e9a4c5dde1da501911b6bcc3d3dad82cc1f65b
Mahmoud Mostafa <m_devolepment@hotmail.com>
2024-12-06 18:45:36 +0200

    Add Icon Slider Section menu

---
+++ Menu/assets/js/main.js
@@ -0,0 +1,11 @@
+const swiper = new Swiper('.swiper', {
+  slidesPerView: 'auto',
+  loop: true,
+  centredSlides: true,
+  speed: 5000,
+  allowTouchMove: true,
+  autoplay: {
+    delay: 0,
+    disabledOnInteraction: false,
+  },
+});
